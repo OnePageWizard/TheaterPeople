8c17c018120028bae1e99ccde70219768ac3d2c5
Maksim Chernenko <maksim.maestto@gmail.com>
2023-06-10 00:02:06 +0300

    Fix GraphQL query

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,50 +1,54 @@
 import * as React from "react"
 import Layout from "../components/Layout/Layout"
 import Carousel from "../components/Carousel/Carousel"
-//import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import "./index.scss"
 
 const IndexPage = () => {
-  // const { allStrapiMainSlide, strapiMainpage } = useStaticQuery(graphql`
-  //   query {
-  //     allStrapiMainSlide {
-  //       nodes {
-  //         id
-  //         Header
-  //         Image {
-  //           localFile {
-  //             childImageSharp {
-  //               gatsbyImageData
-  //             }
-  //           }
-  //         }
-  //         Description
-  //       }
-  //     }
-  //     strapiMainpage {
-  //       Header
-  //       Text {
-  //         data {
-  //           Text
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const { allStrapiMainSlide, strapiMainpage } = useStaticQuery(graphql`
+    query {
+      allStrapiMainSlide {
+        nodes {
+          id
+          Header
+          Image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  quality: 100
+                  breakpoints: [750, 1000, 1366, 1920]
+                  aspectRatio: 1.7
+                  placeholder: BLURRED
+                )
+              }
+            }
+          }
+          Description
+        }
+      }
+      strapiMainpage {
+        Header
+        Text {
+          data {
+            Text
+          }
+        }
+      }
+    }
+  `)
   
   return (
     <Layout>
       <div className="block">
-        {/* <Carousel 
+        <Carousel 
             elems = {allStrapiMainSlide.nodes}
             settings = {{
               // https://react-slick.neostack.com/docs/api
               dots: true,
-              lazyLoad: true,
               infinite: true,
               autoplay: false,
-              speed: 500,
+              speed: 750,
               slidesToShow: 1,
               slidesToScroll: 1,
               arrows: false,
@@ -57,8 +61,6 @@ const IndexPage = () => {
           <h1 className="block__header-about">{strapiMainpage.Header}</h1>
           <p className="block__text-about">{strapiMainpage.Text.data.Text}</p>
         </div>
-        <h1 className="block__header-about">{strapiMainpage.Header}</h1>
-        <p className="block__text-about">{strapiMainpage.Text.data.Text}</p> */}
       </div>
     </Layout>
   )
