27e45522cc2a95ba4ef4ad697cc71a8a5d7aba12
oribon <guardnight62@gmail.com>
2023-06-14 00:37:38 +0300

    Почти сделал трейнинг, там бек нужен

--- frontend/src/pages/Training.js
+++ frontend/src/pages/Training.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout/Layout'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
@@ -6,30 +6,38 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import './Training.scss'
 
 const Training = () => {
+  const [image, setImage] = useState([])
   return (
     <Layout>
-      <div className='training-wrapper'>
-        <div className='training-wrapper__image'>
+      <div className='training__wrapper'>
+        <div className='training__image'>
           <GatsbyImage className='image' image={"#"} alt='#'></GatsbyImage>
           <span className='title'>dfgdfgdfg dfgf</span>
           <span className='about'>dfgdfsfg dfhs gfh sdfdfdfsd</span>
         </div>
-        <div className='training-wrapper__buttons'>
-          <button>Дети</button>
-          <button>Подростки</button>
-          <button>Взрослые</button>
+        <div className='training__buttons'>
+          <button className='child'>Дети</button>
+          <button className='teen'>Подростки</button>
+          <button className='adult'>Взрослые</button>
         </div>
-        <div className='training-wrapper__ages'>
-          <div className='title'>Дети</div>
-          <span className='text'></span>
-        </div>
-        <div className='training-wrapper__ages'>
-          <div className='title'>Подростки</div>
-          <span className='text'></span>
-        </div>
-        <div className='training-wrapper__ages'>
-          <div className='title'>Взрослые</div>
-          <span className='text'></span>
+        <div className='training__content'>
+          <div className='training__content__ages'>
+            <div className='training__content__title'>Дети</div>
+            <span className='training__content__text'>Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и репрезентативная (более частная). Имманентный подход подразумевает отношение к тексту как к автономной реальности, нацеленность на выявление его внутренней структуры. Репрезентативный — рассмотрение текста как особой формы представления информации о внешней тексту действительности.</span>
+          </div>
+          <div className='training__content__ages'>
+            <div className='training__content__title'>Подростки</div>
+            <span className='training__content__text'>Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и репрезентативная (более частная). Имманентный подход подразумевает отношение к тексту как к автономной реальности, нацеленность на выявление его внутренней структуры. Репрезентативный — рассмотрение текста как особой формы представления информации о внешней тексту действительности.</span>
+            <div className='training__content__image'>
+                {image.map((img) => { return(<GatsbyImage image={img.src} alt='#' className={'training__content__image__' + image.indexOf(img)}/>)})}
+              <div className='training__content__image__7'/>
+            </div>
+          </div>
+          <div className='training__content__ages'>
+            <div className='training__content__title'>Взрослые</div>
+            <span className='training__content__text'>Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и репрезентативная (более частная). Имманентный подход подразумевает отношение к тексту как к автономной реальности, нацеленность на выявление его внутренней структуры. Репрезентативный — рассмотрение текста как особой формы представления информации о внешней тексту действительности.</span>
+            <div className='training__content__image'> </div>
+          </div>
         </div>
       </div>
     </Layout>
--- frontend/src/templates/performancePage.js
+++ frontend/src/templates/performancePage.js
@@ -1,37 +1,34 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
-const performancePage = ({ data }) => (
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-)
-
+const performancePage = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>
 
 export const pageQuery = graphql`
-    query ($Title: String) {
-        strapiSpektakli(Title: { eq: $Title }) {
-            Title
-            Premiere
-            ShortDesc
-            Description {
-                data {
-                    Description
-                }
-            }
-            Director
-            Age
-            Cover {
-                localFile {
-                    childImageSharp {
-                        gatsbyImageData(
-                            quality: 100
-                            breakpoints: [750, 1000, 1366, 1920]
-                            placeholder: BLURRED
-                        )
-                    }
-                }
-            }
+  query ($Title: String) {
+    strapiSpektakli(Title: { eq: $Title }) {
+      Title
+      Premiere
+      ShortDesc
+      Description {
+        data {
+          Description
         }
+      }
+      Director
+      Age
+      Cover {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              breakpoints: [750, 1000, 1366, 1920]
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
     }
+  }
 `
 
-export default performancePage 
+export default performancePage
