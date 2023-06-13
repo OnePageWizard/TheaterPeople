d6932a4c524b0a8d5d81bf604337757d52441b0a
oribon <guardnight62@gmail.com>
2023-06-13 12:29:34 +0300

    Training

--- frontend/src/pages/Training.js
+++ frontend/src/pages/Training.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-import { GatsbyImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 
 import './Training.scss'
 
@@ -10,17 +11,25 @@ const Training = () => {
       <div className='training-wrapper'>
         <div className='training-wrapper__image'>
           <GatsbyImage className='image' image={"#"} alt='#'></GatsbyImage>
+          <span className='title'>dfgdfgdfg dfgf</span>
+          <span className='about'>dfgdfsfg dfhs gfh sdfdfdfsd</span>
         </div>
         <div className='training-wrapper__buttons'>
           <button>Дети</button>
           <button>Подростки</button>
           <button>Взрослые</button>
         </div>
-        <div className='training-wrapper__child'>
+        <div className='training-wrapper__ages'>
+          <div className='title'>Дети</div>
+          <span className='text'></span>
         </div>
-        <div className='training-wrapper__teens'>
+        <div className='training-wrapper__ages'>
+          <div className='title'>Подростки</div>
+          <span className='text'></span>
         </div>
-        <div className='training-wrapper__adult'>
+        <div className='training-wrapper__ages'>
+          <div className='title'>Взрослые</div>
+          <span className='text'></span>
         </div>
       </div>
     </Layout>
